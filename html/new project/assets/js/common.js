5cc1046f991cf13796eaece6ede1482d2e6cc05a
white-bear <172149075@qq.com>
2018-05-17 15:18:39 +0800

    ++

--- html/new project/assets/js/common.js
+++ html/new project/assets/js/common.js
@@ -208,7 +208,6 @@ function move(){
 
 
 function fixedTop() {
-    var bodtH = $("body").height()+60;
     $(".mask").show();
     $("body").css({
         overflow: "hidden",
@@ -217,7 +216,7 @@ function fixedTop() {
         right: 0,
         left: 0,
         top: 0,
-        height: bodtH+"px"
+        height: "100%"
     });
 
     $(".pl-bottom-wrapper").css("top","0");
